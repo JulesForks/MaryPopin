f7890b2a2b045b11d74ffd9e06854c6ed6ea3f47
JulesForks <56185296+JulesForks@users.noreply.github.com>
2023-04-30 18:34:40 +0100

    Update UIViewController+MaryPopin.h

--- MaryPopin/UIViewController+MaryPopin.h
+++ MaryPopin/UIViewController+MaryPopin.h
@@ -383,6 +383,9 @@ typedef NS_ENUM(NSInteger, BKTPopinAlignementOption) {
  */
 - (void)setBlurParameters:(BKTBlurParameters *)blurParameters;
 
+- (void)setDismissDimmingViewCompletionBlock:(void(^)())dismissDimmingViewCompletionBlock;
+- (void(^)())dismissDimmingViewCompletionBlock;
+
 /**
  *  An object used to dismiss dimming view.
  *
